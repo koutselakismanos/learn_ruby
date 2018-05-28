b7110222f1d2e2f243b99c6e88b7c0e251b1203b
koutselakismanos <koutselakismanos@gmail.com>
2018-05-28 04:36:17 +0300

    Finished 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+def add(num1, num2)
+    num1+num2
+end
+
+def subtract(num1, num2)
+    num1-num2
+end
+
+def sum(list)
+    sum = 0
+    list.each do |item|
+        sum += item
+    end
+    sum
+end
+
+def multiply(*args)
+    sum=1
+    args.each do |item|
+        sum*=item
+    end
+    sum
+end
+
+def power(num1, num2)
+    num1 ** num2
+end
+
+def factorial(num)
+    if num == 0
+        1
+    else
+        if num>0
+            num * factorial(num-1)
+        end
+    end
+end
