ac65d1ebe0a3509dfd9e471a2abf7e100236c4a7
koutselakismanos <koutselakismanos@gmail.com>
2018-05-28 22:51:29 +0300

    Finished 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
 # write your code here
+    attr_accessor :title
+    def title=(string)
+        result = ''
+
+        count = 0
+        string.split.each do |value|
+            if (count > 0 && value == 'the' || value == 'a' || value == 'an' || value =='and' || value == 'in' || value == 'of')
+                result += value + ' '
+            else
+                result += value[0].upcase + value[1..value.length] + ' '
+            end
+            count += 1
+        end
+
+        @title = result[0..result.length-2]
+    end
 end
